a968d15494563ecbebfe0b4a9fa2a32cb2d92f75
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-12 01:12:28 +0300

    Update call.py

--- AnonX/plugins/call.py
+++ AnonX/plugins/call.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from YukkiMusic import app, Telegram
+from AnonX import app, Telegram
 import random
 
 @app.on_message(filters.command([f"قران", "قرأن", "سوره", "{BOT_USERNAME} قران"],""))
