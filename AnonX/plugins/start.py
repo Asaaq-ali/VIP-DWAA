64c1408c9cf1ec8a27a9aa5ff2ccf6d66ab994c2
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-12 01:25:38 +0300

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -1,1 +1,32 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from AnonX import app, Telegram
+import random
+
+
+@
+ap.ono_mssage(
+    filters.command(["/start","دعائي"],"")
+  & filters.private
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/6ae4c163aac6f69ad9eb9.jpg",
+        caption=f""" **✧اهلين فيك يا عيوني في بوت**\n  ✧** لتفعيل البوت:**\n✧**ضيف البوت لجروبك واكتب تفعيل الادعيه **""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "أضف البوت لجروبك", url=f"https://t.me/dwaalbot?startgroup=true"), 
+                ],[
+                    InlineKeyboardButton(
+                        " 💐𝐒𝐎𝐔𝐑𝐂𝐄 𝐒𝐎𝐇𝐀 💐", url=f"t.me/ASAAQLIO"),
+                ],
+
+            ]
+
+        ),
+
+    )
 
